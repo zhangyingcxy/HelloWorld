6eed4e1109ae863b55a90826ed6f8eb386367789
z_hangYing <1098427532@qq.com>
2021-04-21 18:49:32 +0800

    2021/4/21 汇率计算（页面跳转）

--- app/src/main/java/com/swufe/helloworld/AutoRateActivity.java
+++ app/src/main/java/com/swufe/helloworld/AutoRateActivity.java
@@ -1,46 +1,51 @@
 package com.swufe.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AutoRateActivity extends AppCompatActivity {
+    public static final String EXTRA_DOLLAR="com.swufe.helloworld.MESSAGE1";
+    public static final String EXTRA_EURO="com.swufe.helloworld.MESSAGE2";
+    public static final String EXTRA_HAN="com.swufe.helloworld.MESSAGE3";
 
-    TextView result;
-    EditText et1,et2;
+//    TextView result;
+    EditText et1,et2,et3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_rate);
     }
 
-    Float money,rate,convert;
-    public void Convert(View v){
-        result = (TextView) findViewById(R.id.result);
-        et1 = (EditText) findViewById(R.id.money);
-        et2 = (EditText) findViewById(R.id.rate);
-        String input_1 = et1.getText().toString();
-        String input_2 = et2.getText().toString();
+    Float dollar,euro,han,convert;
+    public void Reset(View v){
+//        result = (TextView) findViewById(R.id.result);
+        et1 = (EditText) findViewById(R.id.dollar);
+        et2 = (EditText) findViewById(R.id.euro);
+        et3 = (EditText) findViewById(R.id.han);
+        String dollar = et1.getText().toString();
+        String euro = et2.getText().toString();
+        String han = et3.getText().toString();
 
-        if (input_1 != null && input_1.length()>0 && input_2 != null && input_2.length()>0){
-             money = Float.parseFloat(input_1);
-             rate =  Float.parseFloat(input_2);
+        if (dollar != null && dollar.length()>0 && euro != null && euro.length()>0 && han != null && han.length()>0){
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_DOLLAR, dollar);
+                intent.putExtra(EXTRA_EURO, euro);
+                intent.putExtra(EXTRA_HAN, han);
+                setResult(RESULT_OK,intent);
+                finish();
+
         }
         else {
             Toast.makeText(this,"请输入内容",Toast.LENGTH_SHORT).show();
         }
 
-        if(v.getId()==R.id.convert){
-            convert = money * rate;
-        String convert_str = String.valueOf(convert);
-        result.setText("结果为："+convert_str);
 
-
-        }
     }
 
 }
--- app/src/main/java/com/swufe/helloworld/RateActivity.java
+++ app/src/main/java/com/swufe/helloworld/RateActivity.java
@@ -3,48 +3,81 @@ package com.swufe.helloworld;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RateActivity extends AppCompatActivity {
 
     TextView result;
     EditText money;
+    Float dollar_rate ;
+    Float euro_rate ;
+    Float han_rate ;
+    Float inpu,convert;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dollar_rate= 0.1539f;
+        euro_rate = 0.1282f;
+        han_rate = 172.21f;
+
         setContentView(R.layout.activity_rate);
     }
 
-    Float inpu,convert;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("dollar", "onActivityResult: ");
+        switch(requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String dollar = data.getStringExtra(AutoRateActivity.EXTRA_DOLLAR);
+                    String euro = data.getStringExtra(AutoRateActivity.EXTRA_EURO);
+                    String han = data.getStringExtra(AutoRateActivity.EXTRA_HAN);
+                    dollar_rate = Float.parseFloat(dollar);
+                    euro_rate = Float.parseFloat(euro);
+                    han_rate = Float.parseFloat(han);
+                    Log.d("dollar", "onActivityResult: "+dollar_rate+euro_rate+han_rate);
+
+                }
+                break;
+            default:
+        }
+    }
+
      public void Convert(View v){
         result = (TextView) findViewById(R.id.result);
         money = (EditText) findViewById(R.id.rate);
         String inp = money.getText().toString();
         if (inp != null && inp.length()>0){
             inpu = Float.parseFloat(inp);
+
         }
         else {
             Toast.makeText(this,"请输入内容",Toast.LENGTH_SHORT).show();
         }
          if(v.getId()==R.id.euro){
-             convert = inpu * 0.1278f;
+             convert = inpu * euro_rate ;
          }else if(v.getId()==R.id.dollar) {
-             convert = 7.0f* inpu;
+             convert = dollar_rate * inpu;
          }
          else{
-             convert = 170.6231f * inpu;
+             convert = han_rate * inpu;
          }
-         String convert_str = String.valueOf(convert);
+//         String convert_str = String.valueOf(convert);
+         String convert_str = String.format("%.2f",convert);
          result.setText(convert_str);
      }
      public void jump(View v){
-         Intent jump = new Intent(this,MainActivity2.class);
-         startActivity(jump);
+         Intent jump = new Intent(RateActivity.this,AutoRateActivity.class);
+         startActivityForResult(jump,1);
      }
      public  void  JD(View v){
          Intent jd= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.jd.com"));

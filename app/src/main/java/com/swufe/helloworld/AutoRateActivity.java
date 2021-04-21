d3313849290c6a6430af87db5e2798ebb5aa399e
z_hangYing <1098427532@qq.com>
2021-04-21 21:56:20 +0800

    2021/4/21 汇率计算（页面跳转与按钮绑定）

--- app/src/main/java/com/swufe/helloworld/AutoRateActivity.java
+++ app/src/main/java/com/swufe/helloworld/AutoRateActivity.java
@@ -2,6 +2,7 @@ package com.swufe.helloworld;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,39 +14,64 @@ public class AutoRateActivity extends AppCompatActivity {
     public static final String EXTRA_EURO="com.swufe.helloworld.MESSAGE2";
     public static final String EXTRA_HAN="com.swufe.helloworld.MESSAGE3";
 
-//    TextView result;
-    EditText et1,et2,et3;
+//
+    private static final String TAG = "AutoRateActivity";
+    EditText et_dollar,et_euro,et_han;
+    float com_dollor,com_euro,com_han;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_rate);
-    }
+        Intent com_intent = getIntent();
+        com_dollor = com_intent.getFloatExtra("dollar_rate_to",0.0f);
+        com_euro = com_intent.getFloatExtra("euro_rate_to",0.0f);
+        com_han = com_intent.getFloatExtra("han_rate_to",0.0f);
 
-    Float dollar,euro,han,convert;
+        et_dollar = (EditText) findViewById(R.id.dollar);
+        et_euro = (EditText) findViewById(R.id.euro);
+        et_han = (EditText) findViewById(R.id.han);
+
+        et_dollar.setText(String.valueOf(com_dollor));
+        et_euro.setText(String.valueOf(com_euro));
+        et_han.setText(String.valueOf(com_han));
+
+        Log.i(TAG, "onCreate: AutoRateActivity com_dollor="+com_dollor);
+        Log.i(TAG, "onCreate: AutoRateActivity com_euro="+com_euro);
+        Log.i(TAG, "onCreate: AutoRateActivity com_han="+com_han);
+
+    }
+    
+
     public void Reset(View v){
-//        result = (TextView) findViewById(R.id.result);
-        et1 = (EditText) findViewById(R.id.dollar);
-        et2 = (EditText) findViewById(R.id.euro);
-        et3 = (EditText) findViewById(R.id.han);
-        String dollar = et1.getText().toString();
-        String euro = et2.getText().toString();
-        String han = et3.getText().toString();
+
+        String dollar = et_dollar.getText().toString();
+        String euro = et_euro.getText().toString();
+        String han = et_han.getText().toString();
+
+        com_dollor = Float.parseFloat(dollar);
+        com_euro = Float.parseFloat(euro);
+        com_han = Float.parseFloat(han);
 
         if (dollar != null && dollar.length()>0 && euro != null && euro.length()>0 && han != null && han.length()>0){
                 Intent intent = new Intent();
-                intent.putExtra(EXTRA_DOLLAR, dollar);
-                intent.putExtra(EXTRA_EURO, euro);
-                intent.putExtra(EXTRA_HAN, han);
-                setResult(RESULT_OK,intent);
+                //方法一
+                Bundle bdl = new Bundle();
+                bdl.putFloat("dollar_key",com_dollor);
+                bdl.putFloat("euro_key",com_euro);
+                bdl.putFloat("han_key",com_han);
+                intent.putExtras(bdl);
+                setResult(3,intent);
+                //方法二
+//                intent.putExtra(EXTRA_DOLLAR, dollar);
+//                intent.putExtra(EXTRA_EURO, euro);
+//                intent.putExtra(EXTRA_HAN, han);
+//                setResult(RESULT_OK,intent);
                 finish();
 
         }
         else {
             Toast.makeText(this,"请输入内容",Toast.LENGTH_SHORT).show();
         }
-
-
     }
-
 }
--- app/src/main/java/com/swufe/helloworld/RateActivity.java
+++ app/src/main/java/com/swufe/helloworld/RateActivity.java
@@ -1,19 +1,22 @@
 package com.swufe.helloworld;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RateActivity extends AppCompatActivity {
 
+    private static final String TAG = "RateActivity";
     TextView result;
     EditText money;
     Float dollar_rate ;
@@ -24,33 +27,40 @@ public class RateActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+        //初始化汇率
         dollar_rate= 0.1539f;
         euro_rate = 0.1282f;
         han_rate = 172.21f;
-
-        setContentView(R.layout.activity_rate);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d("dollar", "onActivityResult: ");
-        switch(requestCode){
-            case 1:
-                if(resultCode == RESULT_OK){
-                    String dollar = data.getStringExtra(AutoRateActivity.EXTRA_DOLLAR);
-                    String euro = data.getStringExtra(AutoRateActivity.EXTRA_EURO);
-                    String han = data.getStringExtra(AutoRateActivity.EXTRA_HAN);
-                    dollar_rate = Float.parseFloat(dollar);
-                    euro_rate = Float.parseFloat(euro);
-                    han_rate = Float.parseFloat(han);
-                    Log.d("dollar", "onActivityResult: "+dollar_rate+euro_rate+han_rate);
-
-                }
-                break;
-            default:
+        if(requestCode==1 && resultCode==3){
+            Bundle bundle = data.getExtras();
+            dollar_rate = bundle.getFloat("dollar_key",0.0f);
+            euro_rate = bundle.getFloat("euro_key",0.0f);
+            han_rate = bundle.getFloat("han_key",0.0f);
         }
+//        switch(requestCode){
+//            case 1:
+//                if(resultCode == RESULT_OK){
+//                    String dollar = data.getStringExtra(AutoRateActivity.EXTRA_DOLLAR);
+//                    String euro = data.getStringExtra(AutoRateActivity.EXTRA_EURO);
+//                    String han = data.getStringExtra(AutoRateActivity.EXTRA_HAN);
+//
+//                    dollar_rate = Float.parseFloat(dollar);
+//                    euro_rate = Float.parseFloat(euro);
+//                    han_rate = Float.parseFloat(han);
+//                    Log.i("dollar", "onActivityResult: "+dollar_rate+euro_rate+han_rate);
+//                }
+//                break;
+//            default:
+//        }
+
     }
+
 
      public void Convert(View v){
         result = (TextView) findViewById(R.id.result);
@@ -75,14 +85,40 @@ public class RateActivity extends AppCompatActivity {
          String convert_str = String.format("%.2f",convert);
          result.setText(convert_str);
      }
+     //跳转到用户自行设置汇率页面
      public void jump(View v){
+        open();
+
+     }
+
+     private void open(){
          Intent jump = new Intent(RateActivity.this,AutoRateActivity.class);
+         jump.putExtra("dollar_rate_to",dollar_rate);
+         jump.putExtra("euro_rate_to",euro_rate);
+         jump.putExtra("han_rate_to",han_rate);
          startActivityForResult(jump,1);
-     }
-     public  void  JD(View v){
-         Intent jd= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.jd.com"));
-         startActivity(jd);
 
+         Log.i(TAG, "jump: dollar_rate="+dollar_rate);
+         Log.i(TAG, "jump: euro_rate="+euro_rate);
+         Log.i(TAG, "jump: han_rate="+han_rate);
      }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.auto_rate,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.settings){
+            open();
+        }
+        else {
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
